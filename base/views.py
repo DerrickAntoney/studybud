d95675724ed4c0c8a0e9fa8b99c4378cd511dc95
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2023-11-06 12:10:20 +0300

    Added routing from home to rooms

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 # Create your views here.
 
 rooms = [{'id': 1, 'name': 'Lets learn python!'},
@@ -10,7 +10,12 @@ rooms = [{'id': 1, 'name': 'Lets learn python!'},
 
 def home(request):
   context = {'rooms': rooms}
-  return render(request, 'home.html', context)
+  return render(request, 'base/home.html', context)
 
-def room(request):
-  return render(request, 'room.html')
+def room(request, pk):
+  room = None
+  for i in rooms:
+    if i['id'] == int(pk):
+      room = i
+    context = {'room': room}
+  return render(request, 'base/room.html', context)
