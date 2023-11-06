4f200ae4f51cde909462b1e2d1778325a9314661
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2023-11-06 08:57:35 +0300

    Add data in the view  to the home html template

--- base/views.py
+++ base/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [{'id': 1, 'name': 'Lets learn python!'},
+         {'id': 2, 'name': 'Django Developers'},
+         {'id': 3, 'name': 'Lets Design'},
+         ]
+
 def home(request):
-  return render(request, 'home.html')
+  context = {'rooms': rooms}
+  return render(request, 'home.html', context)
 
 def room(request):
   return render(request, 'room.html')
