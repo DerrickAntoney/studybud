04e65f0e5a80b9389714c87c711716929885c828
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2023-11-07 11:53:00 +0300

    Fixed bug

--- base/views.py
+++ base/views.py
@@ -3,19 +3,17 @@ from django.http import HttpResponse
 from .models import Room
 # Create your views here.
 
-rooms = [{'id': 1, 'name': 'Lets learn python!'},
-         {'id': 2, 'name': 'Django Developers'},
-         {'id': 3, 'name': 'Lets Design'},
-         ]
+# rooms = [{'id': 1, 'name': 'Lets learn python!'},
+#          {'id': 2, 'name': 'Django Developers'},
+#          {'id': 3, 'name': 'Lets Design'},
+#          ]
 
 def home(request):
+  rooms = Room.objects.all()
   context = {'rooms': rooms}
   return render(request, 'base/home.html', context)
 
 def room(request, pk):
-  room = None
-  for i in rooms:
-    if i['id'] == int(pk):
-      room = i
-    context = {'room': room}
+  room = Room.objects.get(id=pk)
+  context = {'room': room}
   return render(request, 'base/room.html', context)
